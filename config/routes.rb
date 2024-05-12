da2c39f451142721b2ed9ee06b44b811ba7d8e48
tls3254 <bikim9923@gmail.com>
2024-05-12 18:43:02 +0900

    라우트 설정 변경

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-  post "/users/signup", to: "users#signup"
-  post "/auth/signin", to: "users#signin"
-  post "/contents", to: "contents#create"
-  get "/content", to: "contents#show"
-  put "/contents/:id", to: "contents#update"
-  delete "/contents/:id", to: "contents#destroy"
+  post "/api/v1/users/signup", to: "users#signup"
+  post "/api/v1/auth/signin", to: "users#signin"
+  post "/api/v1/contents", to: "contents#create"
+  get "/api/v1/content", to: "contents#show"
+  put "/api/v1/contents/:id", to: "contents#update"
+  delete "api/v1/contents/:id", to: "contents#destroy"
   resources :users
   # Defines the root path route ("/")
   # root "posts#index"
